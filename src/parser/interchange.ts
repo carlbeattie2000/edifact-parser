8ef00421434322329d093a826f8100509dd6fdf4
= <12beattiecastp@gmail.com>
2026-04-26 17:32:47 +0100

    feat: turn Messages and Interchange into plain data containers

--- src/parser/interchange.ts
+++ src/parser/interchange.ts
@@ -1,39 +1,15 @@
 import type Message from "./message.js";
 
 export default class Interchange {
-	public syntaxIdentifier: string;
-	public syntaxVersion: string;
-	public senderId: string;
-	public recipientId: string;
-	public date: string;
-	public time: string;
-	public controlReference: string;
+	public syntaxIdentifier: string = '';
+	public syntaxVersion: string = '';
+	public senderId: string = '';
+	public recipientId: string = '';
+	public date: string = '';
+	public time: string = '';
+	public controlReference: string = '';
 
-	public messages: Message[];
+	public messages: Message[] = [];
 
-	public declaredMessageCount: number;
-
-	constructor(
-		syntaxIndentifier: string,
-		syntaxVersion: string,
-		senderId: string,
-		recipientId: string,
-		date: string,
-		time: string,
-		controlReference: string,
-		messages: Message[],
-		declaredMessageCount: number,
-	) {
-		this.syntaxIdentifier = syntaxIndentifier;
-		this.syntaxVersion = syntaxVersion;
-		this.senderId = senderId;
-		this.recipientId = recipientId;
-		this.date = date;
-		this.time = time;
-		this.controlReference = controlReference;
-
-		this.messages = messages;
-
-		this.declaredMessageCount = declaredMessageCount;
-	}
+	public declaredMessageCount: number = 0;
 }
--- src/parser/message.ts
+++ src/parser/message.ts
@@ -1,36 +1,14 @@
 import type Segment from "./segment.js";
 
 export default class Message {
-	public messageReferenceNumber: string;
-	public messageType: string;
-	public messageVersion: string;
-	public messageRelease: string;
-	public controllingAgency: string;
-	public associationCode: string;
+	public messageReferenceNumber: string = '';
+	public messageType: string = '';
+	public messageVersion: string = '';
+	public messageRelease: string = '';
+	public controllingAgency: string = '';
+	public associationCode: string = '';
 
-	public segments: Segment[];
+	public segments: Segment[] = [];
 
-	public declaredSegmentCount: number;
-
-	constructor(
-		messageReferenceNumber: string,
-		messageType: string,
-		messageVersion: string,
-		messageRelease: string,
-		controllingAgency: string,
-		associationCode: string,
-		segments: Segment[],
-		declaredSegmentCount: number,
-	) {
-		this.messageReferenceNumber = messageReferenceNumber;
-		this.messageType = messageType;
-		this.messageVersion = messageVersion;
-		this.messageRelease = messageRelease;
-		this.controllingAgency = controllingAgency;
-		this.associationCode = associationCode;
-
-		this.segments = segments;
-
-		this.declaredSegmentCount = declaredSegmentCount;
-	}
+	public declaredSegmentCount: number = 0;
 }
