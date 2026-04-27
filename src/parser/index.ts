0787037ab119ebe0a2c6c90e48de89be62d238ab
= <12beattiecastp@gmail.com>
2026-04-27 10:50:37 +0100

    refactor: separate interchange and message parsing into dedicated classes

--- src/parser/index.ts
+++ src/parser/index.ts
@@ -1,12 +1,8 @@
-import {
-	EdifactEnvelopeError,
-	EdifactSyntaxError,
-	EdifactValidationError,
-} from "../errors.js";
+import { EdifactSyntaxError } from "../errors.js";
 import ComponentDataElement from "./component_data_element.js";
 import DataElement from "./data_element.js";
-import Interchange from "./interchange.js";
-import Message from "./message.js";
+import type Interchange from "./interchange.js";
+import InterchangeParser from "./interchange_parser.js";
 import Segment from "./segment.js";
 
 export default class Parser {
@@ -135,230 +131,53 @@ export default class Parser {
 		return parsedSegments;
 	}
 
-	private isInterchangeHeader(segment: Segment): boolean {
-		return segment.tag === "UNB";
-	}
-
-	private isInterchangeTrailer(segment: Segment): boolean {
-		return segment.tag === "UNZ";
-	}
-
-	private strictModeInterchangeCheck(segments: Segment[]) {
-		if (!this.strict) {
-			return;
-		}
-
-		const head = segments[0];
-		const tail = segments[segments.length - 1];
-
-		if (!head || !this.isInterchangeHeader(head)) {
-			throw new EdifactEnvelopeError();
-		}
-
-		if (!tail || !this.isInterchangeTrailer(tail)) {
-			throw new EdifactEnvelopeError();
-		}
-
-		let headerCount = 0;
-		let trailerCount = 0;
-
-		for (const segment of segments) {
-			if (this.isInterchangeHeader(segment)) {
-				headerCount++;
-			}
-
-			if (this.isInterchangeTrailer(segment)) {
-				trailerCount++;
-			}
-		}
-
-		if (headerCount > 1 || trailerCount > 1) {
-			throw new EdifactEnvelopeError();
-		}
-	}
-
-	private parseInterchangeHeader(segment: Segment): Interchange {
-		const interchange = new Interchange();
-		interchange.syntaxIdentifier =
-			segment.getDataElement(0)?.getComponent(0)?.value ?? "";
-		interchange.syntaxVersion =
-			segment.getDataElement(0)?.getComponent(1)?.value ?? "";
-		interchange.senderId = segment.getDataElement(1)?.Value ?? "";
-		interchange.recipientId = segment.getDataElement(2)?.Value ?? "";
-		interchange.date = segment.getDataElement(3)?.getComponent(0)?.value ?? "";
-		interchange.time = segment.getDataElement(3)?.getComponent(1)?.value ?? "";
-		interchange.controlReference = segment.getDataElement(4)?.Value ?? "";
-		return interchange;
-	}
-
-	private parseInterchangeTrailer(segment: Segment, interchange: Interchange) {
-		interchange.declaredMessageCount = Number(
-			segment.getDataElement(0)?.Value ?? 0,
-		);
-		const controlReference = segment.getDataElement(1)?.Value ?? "";
-
-		if (this.strict && controlReference !== interchange.controlReference) {
-			throw new EdifactValidationError();
-		}
-	}
-
-	private isMessageHeader(segment: Segment): boolean {
-		return segment.tag === "UNH";
-	}
-
-	private isMeassgeTrailer(segment: Segment): boolean {
-		return segment.tag === "UNT";
-	}
-
-	private strictModeMessagesCheck(segments: Segment[]): void {
-		let start = -1;
-		let end = -1;
-		let index = 0;
-
-		for (const segment of segments) {
-			if (this.isMessageHeader(segment)) {
-				if (start > end) {
-					throw new EdifactEnvelopeError();
-				}
-				start = index;
-			}
-
-			if (this.isMeassgeTrailer(segment)) {
-				if (start < end) {
-					throw new EdifactEnvelopeError();
-				}
-				end = index;
-			}
-
-			index++;
-		}
-
-		if (start > end) {
-			throw new EdifactEnvelopeError();
-		}
-	}
-
-	private parseMessageHeader(segment: Segment): Message {
-		const message = new Message();
-		message.messageReferenceNumber = segment.getDataElement(0)?.Value ?? "";
-		const messageIdentifier = segment.getDataElement(1);
-		message.messageType = messageIdentifier?.getComponent(0)?.value ?? "";
-		message.messageVersion = messageIdentifier?.getComponent(1)?.value ?? "";
-		message.messageRelease = messageIdentifier?.getComponent(2)?.value ?? "";
-		message.controllingAgency = messageIdentifier?.getComponent(3)?.value ?? "";
-		message.associationCode = messageIdentifier?.getComponent(4)?.value ?? "";
-		return message;
-	}
-
-	private parseMessageTrailer(segment: Segment, message: Message): void {
-		const declaredSegmentCount = Number(segment.getDataElement(0)?.Value ?? 0);
-		const controlReference = segment.getDataElement(1)?.Value ?? "";
-
-		if (this.strict && controlReference !== message.messageReferenceNumber) {
-			throw new EdifactValidationError();
-		}
-
-		message.declaredSegmentCount = declaredSegmentCount;
-	}
-
-	private preValidate(segments: Segment[]): void {
+	private preValidate(
+		segments: Segment[],
+		checks: ((segments: Segment[]) => void)[],
+	): void {
 		if (!this.strict) return;
 
-		this.strictModeInterchangeCheck(segments);
-		this.strictModeMessagesCheck(segments);
+		checks.forEach((check) => {
+			check(segments);
+		});
 	}
 
-  private postValidate(interchanges: Interchange[]) {
-    if (!this.strict) return;
-
-    for (const interchange of interchanges) {
-      if (interchange.declaredMessageCount !== interchange.messages.length) {
-        throw new EdifactValidationError();
-      }
-
-      for (const message of interchange.messages) {
-        if (message.declaredSegmentCount !== message.segments.length + 2) {
-          throw new EdifactValidationError();
-        }
-      }
-    }
-  }
-
 	public interchanges(segments: Segment[]) {
-		this.preValidate(segments);
+		const interchangeParser = new InterchangeParser(this.strict);
 
-		const interchanges: Interchange[] = [];
-		let currentInterchange: Interchange | null = null;
-		let currentMessage: Message | null = null;
+		this.preValidate(segments, [
+			interchangeParser.strictModeInterchangeCheck.bind(interchangeParser),
+			interchangeParser.messageParser.strictModeMessagesCheck.bind(interchangeParser.messageParser),
+		]);
 
 		for (const segment of segments) {
-			if (this.isInterchangeHeader(segment)) {
-				if (currentInterchange) {
-          if (currentMessage) {
-            currentInterchange.messages.push(currentMessage);
-            currentMessage = null;
-          }
-					interchanges.push(currentInterchange);
-					currentInterchange = null;
-				}
-
-				currentInterchange = this.parseInterchangeHeader(segment);
+			if (interchangeParser.isInterchangeHeader(segment)) {
+				interchangeParser.handleInterchangeOpen(segment);
 				continue;
 			}
 
-			if (this.isInterchangeTrailer(segment)) {
-				if (currentInterchange) {
-					this.parseInterchangeTrailer(segment, currentInterchange);
-          if (currentMessage) {
-            currentInterchange.messages.push(currentMessage);
-            currentMessage = null;
-          }
-					interchanges.push(currentInterchange);
-					currentInterchange = null;
-				}
+			if (interchangeParser.isInterchangeTrailer(segment)) {
+				interchangeParser.handleInterchangeClose(segment);
 				continue;
 			}
 
-			if (!currentInterchange) {
-				currentInterchange = new Interchange();
-			}
+			interchangeParser.prepareForMessages();
 
-			if (this.isMessageHeader(segment)) {
-				if (currentMessage) {
-					currentInterchange.messages.push(currentMessage);
-					currentMessage = null;
-				}
-
-				currentMessage = this.parseMessageHeader(segment);
+			if (interchangeParser.messageParser.isMessageHeader(segment)) {
+				interchangeParser.messageParser.handleOpenMessage(segment);
 				continue;
 			}
 
-			if (this.isMeassgeTrailer(segment)) {
-				if (currentMessage) {
-					this.parseMessageTrailer(segment, currentMessage);
-					currentInterchange.messages.push(currentMessage);
-					currentMessage = null;
-				}
+			if (interchangeParser.messageParser.isMessageTrailer(segment)) {
+				interchangeParser.messageParser.handleCloseMessage(segment);
 				continue;
 			}
 
-			if (!currentMessage) {
-				currentMessage = new Message();
-			}
-
-      currentMessage.segments.push(segment);
+			interchangeParser.messageParser.prepareForSegments();
+			interchangeParser.messageParser.receiveSegment(segment);
 		}
 
-		if (currentInterchange) {
-			if (currentMessage) {
-				currentInterchange.messages.push(currentMessage);
-				currentMessage = null;
-			}
-			interchanges.push(currentInterchange);
-			currentInterchange = null;
-		}
-
-    this.postValidate(interchanges);
+		const interchanges = interchangeParser.terminate();
 
 		return interchanges;
 	}
