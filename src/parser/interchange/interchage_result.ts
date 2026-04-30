6a2855017580291231a991db0c2f8c1a7e410dbf
Carl Beattie <contact.carlbeattie@gmail.com>
2026-04-30 02:51:54 +0100

    feat: add InterchangeResult skeleton

---
+++ src/parser/interchange/interchage_result.ts
@@ -0,0 +1,33 @@
+import type Interchange from "./interchange.js";
+
+export default class InterchangeResult {
+  #values: Interchange[];
+  #errors: Error[];
+
+  public first() {
+  }
+
+  public firstOrFail() {
+  }
+
+  public at() {
+  }
+
+  public atOrFail() {
+  }
+
+  public length() {
+  }
+
+  public all() {
+  }
+
+  public errors() {
+  }
+
+  public hasErrors() {
+  }
+
+  public isValid() {
+  }
+}
